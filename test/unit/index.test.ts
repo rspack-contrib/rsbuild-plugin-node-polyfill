bcd474af8a0813f188be98e62eaca888a1d5dc90
neverland <chenjiahan.jait@bytedance.com>
2025-01-30 19:56:06 +0800

    test: use Node.js native test module

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,174 +1,171 @@
-import { expect, test } from 'vitest';
+import assert from 'node:assert/strict';
+import test from 'node:test';
 import {
 	builtinMappingResolved,
 	getResolveFallback,
 	resolvePolyfill,
 } from '../../src/index';
 
-test('resolvePolyfill', () => {
-	expect(resolvePolyfill('fs', { fs: 'memfs' })).toBe('memfs');
-	expect(resolvePolyfill('fs')).toBe(null);
-	expect(resolvePolyfill('buffer')).toBe(builtinMappingResolved.buffer);
+test('resolvePolyfill', async (t) => {
+	assert.equal(resolvePolyfill('fs', { fs: 'memfs' }), 'memfs');
+	assert.equal(resolvePolyfill('fs'), null);
+	assert.equal(resolvePolyfill('buffer'), builtinMappingResolved.buffer);
 });
 
-test('getResolveFallback', () => {
+test('getResolveFallback', async (t) => {
 	const defaultFallback = getResolveFallback({});
 	const withProtocolImportsFallback = getResolveFallback({
 		protocolImports: true,
 	});
 
-	expect(Object.keys(defaultFallback)).toMatchInlineSnapshot(`
-		[
-		  "assert",
-		  "buffer",
-		  "child_process",
-		  "cluster",
-		  "console",
-		  "constants",
-		  "crypto",
-		  "dgram",
-		  "dns",
-		  "domain",
-		  "events",
-		  "fs",
-		  "http",
-		  "https",
-		  "module",
-		  "net",
-		  "os",
-		  "path",
-		  "punycode",
-		  "process",
-		  "querystring",
-		  "readline",
-		  "repl",
-		  "stream",
-		  "_stream_duplex",
-		  "_stream_passthrough",
-		  "_stream_readable",
-		  "_stream_transform",
-		  "_stream_writable",
-		  "string_decoder",
-		  "sys",
-		  "timers",
-		  "tls",
-		  "tty",
-		  "url",
-		  "util",
-		  "vm",
-		  "zlib",
-		]
-	`);
+	// 替换快照测试为实际的数组比较
+	assert.deepEqual(Object.keys(defaultFallback), [
+		'assert',
+		'buffer',
+		'child_process',
+		'cluster',
+		'console',
+		'constants',
+		'crypto',
+		'dgram',
+		'dns',
+		'domain',
+		'events',
+		'fs',
+		'http',
+		'https',
+		'module',
+		'net',
+		'os',
+		'path',
+		'punycode',
+		'process',
+		'querystring',
+		'readline',
+		'repl',
+		'stream',
+		'_stream_duplex',
+		'_stream_passthrough',
+		'_stream_readable',
+		'_stream_transform',
+		'_stream_writable',
+		'string_decoder',
+		'sys',
+		'timers',
+		'tls',
+		'tty',
+		'url',
+		'util',
+		'vm',
+		'zlib',
+	]);
 
-	expect(Object.keys(withProtocolImportsFallback)).toMatchInlineSnapshot(`
-		[
-		  "assert",
-		  "node:assert",
-		  "buffer",
-		  "node:buffer",
-		  "child_process",
-		  "node:child_process",
-		  "cluster",
-		  "node:cluster",
-		  "console",
-		  "node:console",
-		  "constants",
-		  "node:constants",
-		  "crypto",
-		  "node:crypto",
-		  "dgram",
-		  "node:dgram",
-		  "dns",
-		  "node:dns",
-		  "domain",
-		  "node:domain",
-		  "events",
-		  "node:events",
-		  "fs",
-		  "node:fs",
-		  "http",
-		  "node:http",
-		  "https",
-		  "node:https",
-		  "module",
-		  "node:module",
-		  "net",
-		  "node:net",
-		  "os",
-		  "node:os",
-		  "path",
-		  "node:path",
-		  "punycode",
-		  "node:punycode",
-		  "process",
-		  "node:process",
-		  "querystring",
-		  "node:querystring",
-		  "readline",
-		  "node:readline",
-		  "repl",
-		  "node:repl",
-		  "stream",
-		  "node:stream",
-		  "_stream_duplex",
-		  "node:_stream_duplex",
-		  "_stream_passthrough",
-		  "node:_stream_passthrough",
-		  "_stream_readable",
-		  "node:_stream_readable",
-		  "_stream_transform",
-		  "node:_stream_transform",
-		  "_stream_writable",
-		  "node:_stream_writable",
-		  "string_decoder",
-		  "node:string_decoder",
-		  "sys",
-		  "node:sys",
-		  "timers",
-		  "node:timers",
-		  "tls",
-		  "node:tls",
-		  "tty",
-		  "node:tty",
-		  "url",
-		  "node:url",
-		  "util",
-		  "node:util",
-		  "vm",
-		  "node:vm",
-		  "zlib",
-		  "node:zlib",
-		]
-	`);
+	assert.deepEqual(Object.keys(withProtocolImportsFallback), [
+		'assert',
+		'node:assert',
+		'buffer',
+		'node:buffer',
+		'child_process',
+		'node:child_process',
+		'cluster',
+		'node:cluster',
+		'console',
+		'node:console',
+		'constants',
+		'node:constants',
+		'crypto',
+		'node:crypto',
+		'dgram',
+		'node:dgram',
+		'dns',
+		'node:dns',
+		'domain',
+		'node:domain',
+		'events',
+		'node:events',
+		'fs',
+		'node:fs',
+		'http',
+		'node:http',
+		'https',
+		'node:https',
+		'module',
+		'node:module',
+		'net',
+		'node:net',
+		'os',
+		'node:os',
+		'path',
+		'node:path',
+		'punycode',
+		'node:punycode',
+		'process',
+		'node:process',
+		'querystring',
+		'node:querystring',
+		'readline',
+		'node:readline',
+		'repl',
+		'node:repl',
+		'stream',
+		'node:stream',
+		'_stream_duplex',
+		'node:_stream_duplex',
+		'_stream_passthrough',
+		'node:_stream_passthrough',
+		'_stream_readable',
+		'node:_stream_readable',
+		'_stream_transform',
+		'node:_stream_transform',
+		'_stream_writable',
+		'node:_stream_writable',
+		'string_decoder',
+		'node:string_decoder',
+		'sys',
+		'node:sys',
+		'timers',
+		'node:timers',
+		'tls',
+		'node:tls',
+		'tty',
+		'node:tty',
+		'url',
+		'node:url',
+		'util',
+		'node:util',
+		'vm',
+		'node:vm',
+		'zlib',
+		'node:zlib',
+	]);
 
-	expect(Object.keys(withProtocolImportsFallback).length).toBe(
+	assert.equal(
+		Object.keys(withProtocolImportsFallback).length,
 		Object.keys(defaultFallback).length * 2,
 	);
 
 	const excludeFs = getResolveFallback({ exclude: ['fs'] });
-	expect(
+	assert.deepEqual(
 		Object.keys(defaultFallback).filter(
 			(k) => !Object.keys(excludeFs).includes(k),
 		),
-	).toStrictEqual(['fs']);
-
-	expect(Object.keys(getResolveFallback({ exclude: ['fs'] }))).not.toContain(
-		'fs',
+		['fs'],
 	);
 
-	expect(Object.keys(getResolveFallback({ include: ['fs'] }))).toStrictEqual([
+	assert.ok(
+		!Object.keys(getResolveFallback({ exclude: ['fs'] })).includes('fs'),
+	);
+
+	assert.deepEqual(Object.keys(getResolveFallback({ include: ['fs'] })), [
 		'fs',
 	]);
 
 	const overrides = getResolveFallback({ overrides: { fs: 'memfs' } });
-	expect(overrides.fs).toStrictEqual('memfs');
-	expect({ ...overrides, fs: defaultFallback.fs }).toStrictEqual(
-		defaultFallback,
-	);
+	assert.equal(overrides.fs, 'memfs');
+	assert.deepEqual({ ...overrides, fs: defaultFallback.fs }, defaultFallback);
 
-	expect(() =>
-		getResolveFallback({ include: ['fs'], exclude: ['path'] }),
-	).toThrowErrorMatchingInlineSnapshot(
-		'[Error: `include` is mutually exclusive with `exclude`.]',
+	assert.throws(
+		() => getResolveFallback({ include: ['fs'], exclude: ['path'] }),
+		{ message: '`include` is mutually exclusive with `exclude`.' },
 	);
 });
